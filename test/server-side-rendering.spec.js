f439e70a3f26df423d5f26814a503988320d7077
nature613 <chikonakamura613@gmail.com>
2017-08-24 13:37:20 +0200

    Start a server for the acceptance test
    This allows faster feedback by removing the manual server restart and makes
    result less surprising when you forget to do that.

--- test/server-side-rendering.spec.js
+++ test/server-side-rendering.spec.js
@@ -3,9 +3,15 @@ const unexpectedDOM = require('unexpected-dom');
 const expect = unexpected.clone().use(unexpectedDOM);
 const fetch = require('isomorphic-fetch');
 
+const createServer = require('../server.js');
+
 describe('Vue SSR', () => {
+    const port = 3001;
+    const server = createServer();
+    server.listen(3001);
+
     it('returns 200 with HTML on POST /lemma-widget', async () => {
-        const response = await fetch('http://localhost:3000/lemma-widget', {
+        const response = await fetch(`http://localhost:${port}/lemma-widget`, {
             method: 'POST',
             contentType: 'application/json',
             body: JSON.stringify({})
