180fe4b896ccb02d36f3d0d593b1a18bef61fc13
nature613 <chikonakamura613@gmail.com>
2017-08-29 12:39:10 +0200

    Add template to component
    This previously lived in the PHP view and had to be duplicated in JS unit tests.
    Now it is home, and the problems go away :)

--- src/app/widgets/LemmaWidget.newLemmaWidget.js
+++ src/app/widgets/LemmaWidget.newLemmaWidget.js
@@ -1,3 +1,44 @@
+const template = `<div class="lemma-widget">
+	<ul v-if="!inEditMode" class="lemma-widget_lemma-list">
+		<li v-for="lemma in lemmas" class="lemma-widget_lemma">
+			<span class="lemma-widget_lemma-value">{{lemma.value}}</span>
+			<span class="lemma-widget_lemma-language">{{lemma.language}}</span>
+		</li>
+	</ul>
+	<div v-else class="lemma-widget_edit-area">
+		<ul class="lemma-widget_lemma-list">
+			<li v-for="lemma in lemmas" class="lemma-widget_lemma-edit-box">
+				<span class="lemma-widget_lemma-value-label">
+					{{'wikibase-lemma-field-lemma-label'|message}}
+				</span>
+				<input size="1" class="lemma-widget_lemma-value-input" 
+					v-model="lemma.value" :disabled="isSaving">
+				<span class="lemma-widget_lemma-language-label">
+					{{'wikibase-lemma-field-language-label'|message}}
+				</span>
+				<input size="1" class="lemma-widget_lemma-language-input" 
+					v-model="lemma.language" :disabled="isSaving">
+				<button class="lemma-widget_lemma-remove" v-on:click="remove(lemma)" 
+					:disabled="isSaving" :title="'wikibase-remove'|message">
+					&times;
+				</button>
+			</li>
+			<li>
+				<button type="button" class="lemma-widget_add" v-on:click="add" 
+					:disabled="isSaving" :title="'wikibase-add'|message">+</button>
+			</li>
+		</ul>
+	</div>
+	<div class="lemma-widget_controls" v-if="isInitialized" >
+		<button type="button" class="lemma-widget_edit" v-if="!inEditMode" 
+			:disabled="isSaving" v-on:click="edit">{{'wikibase-edit'|message}}</button>
+		<button type="button" class="lemma-widget_save" v-if="inEditMode" 
+			:disabled="isSaving" v-on:click="save">{{'wikibase-save'|message}}</button>
+		<button type="button" class="lemma-widget_cancel" v-if="inEditMode" 
+			:disabled="isSaving"  v-on:click="cancel">{{'wikibase-cancel'|message}}</button>
+	</div>
+</div>`;
+
 module.exports = ( function ( mw ) {
 	'use strict';
 
